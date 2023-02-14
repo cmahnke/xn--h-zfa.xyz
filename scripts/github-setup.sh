8732c308d8a931c7bd528e9cb56c641bc36d4af3
Christian Mahnke <cmahnke@gmail.com>
2023-02-14 20:07:26 +0100

    Initial commit

---
+++ scripts/github-setup.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+CTX_PATH="$(dirname $(realpath $0))"
+
+$CTX_PATH/../themes/projektemacher-base/scripts/github/setup-dependencies.sh
+$CTX_PATH/../themes/projektemacher-base/scripts/github/docker-images.sh
+$CTX_PATH/../themes/projektemacher-base/scripts/github/dart-sass.sh
---
+++ scripts/post-build/qr-codes.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+python themes/projektemacher-base/scripts/generate-qr-codes.py -i ./content/future/sylt/design/icons/ -b circle
