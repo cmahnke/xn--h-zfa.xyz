7003aec33dccee65133d587214e7cb474ca463ff
Christian Mahnke <cmahnke@gmail.com>
2023-02-15 17:50:55 +0100

    Add python dependencies

--- scripts/github-setup.sh
+++ scripts/github-setup.sh
@@ -4,4 +4,5 @@ CTX_PATH="$(dirname $(realpath $0))"
 
 $CTX_PATH/../themes/projektemacher-base/scripts/github/setup-dependencies.sh
 $CTX_PATH/../themes/projektemacher-base/scripts/github/docker-images.sh
+$CTX_PATH/../themes/projektemacher-base/scripts/github/python-dependencies.sh
 $CTX_PATH/../themes/projektemacher-base/scripts/github/dart-sass.sh
