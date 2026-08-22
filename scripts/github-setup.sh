ab757904a7a13e6cc8ba0c4c401a6c092da6ebc8
Christian Mahnke <194820+cmahnke@users.noreply.github.com>
2026-08-22 20:55:46 +0200

    Fail on error

--- scripts/github-setup.sh
+++ scripts/github-setup.sh
@@ -1,11 +1,10 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-set -e
+set -e -o pipefail
 
 CTX_PATH="$(dirname $(realpath $0))"
 
 $CTX_PATH/../themes/projektemacher-base/scripts/github/jxl-0.12.0.sh
-
 $CTX_PATH/../themes/projektemacher-base/scripts/github/setup-dependencies.sh
 $CTX_PATH/../themes/projektemacher-base/scripts/github/docker-images.sh
 $CTX_PATH/../themes/projektemacher-base/scripts/github/python-dependencies.sh
